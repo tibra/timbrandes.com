ce1437e2c9333ed9ff0f66da77261ab1f162a1cd
Tim Brandes <tb@zinkler-brandes.com>
2018-01-05 13:39:00 +0100

    .

--- Gemfile
+++ Gemfile
@@ -11,3 +11,6 @@ group :jekyll_plugins do
   gem "jekyll-feed", "~> 0.6"
   gem "jekyll-livereload"
 end
+
+gem "capistrano"
+gem "rvm-capistrano"
---
+++ config/deploy.rb
@@ -0,0 +1,50 @@
+# -*- encoding : utf-8 -*-
+# RVM integration
+require "rvm/capistrano"
+
+set :application, "timbrandes"
+set :domain, "84.200.65.138"
+set :use_sudo, false
+set :rvm_ruby_string, "ruby-2.2.1@global"
+set :rvm_type, :user
+set :rvm_install_with_sudo, false
+set :scm, :git
+set :repository, "git@gitlab.zinkler-brandes.com:zb/timbrandescom.git"
+set :deploy_via, :remote_cache
+set :keep_releases, 3
+
+# We have all components of the app on the same server
+server domain, :app, :web, :db, :primary => true
+# Answer yes to questions
+set :default_run_options, {:pty => true}
+
+set :branch, "master"
+set :user, "static_timbrandes"
+# Execute "bundle install" after deploy, but only when really needed
+set :bundle_cmd, "/home/#{user}/.rvm/gems/#{rvm_ruby_string}/bin/bundle"
+set :bundle_dir, "/home/#{user}/application/shared/bundle"
+set :bundle_flags,    "--quiet --deployment"
+set :deploy_to, "/home/#{user}/application"
+
+
+# Must be down here under set :bundle_dir
+require "bundler/capistrano"
+
+# Install RVM and Ruby before deploy
+before "deploy:setup", "rvm:install_rvm"
+before "deploy:setup", "rvm:install_ruby"
+after "deploy:setup", "deploy:touch_configs"
+
+after "deploy", "deploy:restart"
+
+# Clean-up old releases
+after "deploy:restart", "deploy:cleanup"
+
+
+namespace :deploy do
+
+  task :touch_configs do
+    run "mkdir -p #{shared_path}/log"
+  end
+
+end
